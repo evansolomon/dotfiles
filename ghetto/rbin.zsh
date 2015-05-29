ce527304b338072c11fa3b13fbb640ccfc648140
Evan Solomon <evan@evanalyze.com>
2015-05-29 10:22:08 -0700

    Copy requestb.in URL to the clipboard

--- ghetto/rbin.zsh
+++ ghetto/rbin.zsh
@@ -1,6 +1,16 @@
 function rbin {
- http post requestb.in/api/v1/bins | \
-   jq .name | \
-   xargs -I {} echo "http://requestb.in/{}?inspect" | \
-   xargs open
+  # Create bin and parse its name
+  name="$(http post requestb.in/api/v1/bins | jq --raw-output .name)"
+
+  # Form a URL
+  url="http://requestb.in/${name}?inspect"
+
+  # Copy the URL
+  echo "${url}" | pbcopy
+
+  # Open the URL in a browser
+  open "${url}"
+
+  # Print the URL to stdout
+  echo "${url}"
 }
