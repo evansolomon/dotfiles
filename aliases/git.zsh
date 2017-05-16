1ea702255978697217d50f85d26f7c60768c2fe9
Evan Solomon <esolomon@linkedin.com>
2017-05-16 09:12:38 -0700

    Add g=git and better rg

--- aliases/git.zsh
+++ aliases/git.zsh
@@ -1,3 +1,5 @@
+alias g="git"
+
 alias gpr="git pull --rebase"
 alias gitpull="git pull && git submodule update --init --recursive && git submodule foreach git pull origin master"
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset %Cblue[%an]%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)%Creset' --abbrev-commit"
